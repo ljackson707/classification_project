deb41064944e7aa5c2e265a61c5a46c1766033a0
Liam Jackson <ljackson3613@yahoo.com>
2021-03-06 14:18:34 -0600

    fixed acquire_telco_data

--- acquire.py
+++ acquire.py
@@ -1,4 +1,4 @@
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+d#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Imports
 
 import pandas as pd 
@@ -10,5 +10,5 @@ import os
 # For Telco data
 
 def acquire_telco_data(df):
-    telco_df = pd.read_csv("Cust_Churn_Telco.csv")
-    return telco_df
+    df = pd.read_csv("Cust_Churn_Telco.csv")
+    return df
