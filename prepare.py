5d25a3a072a8a2a65d44d5fc6c4f9681d5672450
Liam Jackson <ljackson3613@yahoo.com>
2021-03-07 17:14:30 -0600

    _

--- prepare.py
+++ prepare.py
@@ -37,14 +37,19 @@ def rename_columns(df):
 def remove_columns(df):
     return df.drop(columns=[''])
 
-def train_validate_test_split(df, seed=123):
-    train_and_validate, test = train_test_split(
-        df, test_size=0.2, random_state=seed, stratify=df.chrun
-    )
-    train, validate = train_test_split(
-        train_and_validate,
-        test_size=0.3,
-        random_state=seed,
-        stratify=train_and_validate.churn,
-    )
+def train_validate_test_split(df, target, seed=123):
+    '''
+    This function takes in a dataframe, the name of the target variable
+    (for stratification purposes), and an integer for a setting a seed
+    and splits the data into train, validate and test. 
+    Test is 20% of the original dataset, validate is .30*.80= 24% of the 
+    original dataset, and train is .70*.80= 56% of the original dataset. 
+    The function returns, in this order, train, validate and test dataframes. 
+    '''
+    train_validate, test = train_test_split(df, test_size=0.2, 
+                                            random_state=seed, 
+                                            stratify=df[target])
+    train, validate = train_test_split(train_validate, test_size=0.3, 
+                                       random_state=seed,
+                                       stratify=train_validate[target])
     return train, validate, test
