89bc34dcd2d48f0ee96df53eec35f3283c42d96d
Liam Jackson <ljackson3613@yahoo.com>
2021-03-07 17:47:55 -0600

    Fixed function

--- acquire.py
+++ acquire.py
@@ -1,4 +1,4 @@
-d#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Imports
 
 import pandas as pd 
@@ -10,6 +10,7 @@ import os
 # For Telco data
 
 def acquire_telco_data(df):
+    df = "Cust_Churn_Telco.csv"
     df = pd.read_csv("Cust_Churn_Telco.csv")
     df = pd.DataFrame(df)
     return df
