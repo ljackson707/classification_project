260339bd933aad8eece1bc9675c95542caac0c3d
Liam Jackson <ljackson3613@yahoo.com>
2021-03-08 12:29:57 -0600

    prepare.py

--- prepare.py
+++ prepare.py
@@ -21,21 +21,43 @@ from sklearn.impute import SimpleImputer
 
 def prep_telco_data(df):
     df = df
-    dummy_df = pd.get_dummies(df[["InternetService"]], drop_first=True)
+    df = rename_columns(df)
+    dummy_df = pd.get_dummies(df[["internet_service"]], drop_first=True)
     df = pd.concat([df, dummy_df], axis=1)
+    dummy_df2 = pd.get_dummies(df[["gender"]], drop_first=True)
+    df = pd.concat([df, dummy_df2], axis=1)
+    dummy_df3 = pd.get_dummies(df[["payment_method", "contract"]], drop_first=True)
+    df = pd.concat([df, dummy_df3], axis=1)
+    dummy_df4 = pd.get_dummies(df[["partner", "dependents", "phone_service", "multiple_lines",
+                                    "online_security",'online_backup', 'device_protection','Churn',
+                                    'tech_support','streaming_movies','streaming_tv','paperless_billing']], drop_first=True)
+    df = pd.concat([df, dummy_df4], axis=1)
+    df = remove_columns(df)
     return (df)
 
-def handle_missing_values(df):
-    return df.assign(
-        embark_town=df.embark_town.fillna('Other'),
-        embarked=df.embarked.fillna('O'),
-    )
-
 def rename_columns(df):
-    return df.rename(columns={'':'',})
+    return df.rename(columns={'customerID':'customer_id',
+                                'SeniorCitizen':'senior_citizen',
+                                'Partner':'partner',
+                                'Dependents':'dependents',
+                                'PhoneService':'phone_service',
+                                'MultipleLines':'multiple_lines',
+                                'InternetService':'internet_service',
+                                'OnlineSecurity':'online_security',
+                                'StreamingTV':'streaming_tv',
+                                'StreamingMovies':'streaming_movies',
+                                'Contract':'contract',
+                                'PaperlessBilling':'paperless_billing',
+                                'PaymentMethod':'payment_method',
+                                'MonthlyCharges':'monthly_charges',
+                                'TotalCharges':'total_charges',
+                                'OnlineBackup':'online_backup',
+                                'DeviceProtection':'device_protection',
+                                'TechSupport':'tech_support'})
 
 def remove_columns(df):
-    return df.drop(columns=[''])
+    return df.drop(columns=['internet_service', 'gender', 'payment_method', 'contract',"partner", "dependents", "phone_service", "multiple_lines",
+                            "online_security",'online_backup', 'device_protection','Churn','tech_support','streaming_movies','streaming_tv','paperless_billing'])
 
 def train_validate_test_split(df, target, seed=123):
     '''
